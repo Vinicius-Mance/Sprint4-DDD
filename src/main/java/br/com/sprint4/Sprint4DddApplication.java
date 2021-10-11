76abbe19d5ed39d8a3457c2cf043e7baf2312e7d
Vinicius-Mance <vinimanceprof@gmail.com>
2021-10-11 18:37:08 -0300

    commit inicial

---
+++ src/main/java/br/com/sprint4/Sprint4DddApplication.java
@@ -0,0 +1,13 @@
+package br.com.sprint4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprint4DddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprint4DddApplication.class, args);
+	}
+
+}
