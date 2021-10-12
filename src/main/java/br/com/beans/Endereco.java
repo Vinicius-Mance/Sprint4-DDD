c12df7a19f8a4ccb6cc009386effb8d0b3148e6c
Vinicius-Mance <vinimanceprof@gmail.com>
2021-10-12 01:00:48 -0300

    implementação banco de dados

--- src/main/java/br/com/beans/Endereco.java
+++ src/main/java/br/com/beans/Endereco.java
@@ -1,18 +1,50 @@
 package br.com.beans;
 
-public class Endereco {
+import javax.persistence.*;
 
+@Entity
+@Table(name="endereco")
+public class Endereco {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "codigo")
+    private int codigo;
+
+    @Column(name="cep")
     private String cep;
+
+    @Column(name="logradouro")
     private String logradouro;
+
+    @Column(name="complemento")
     private String complemento;
+
+    @Column(name="bairro")
     private String bairro;
+
+    @Column(name="localidade")
     private String localidade;
+
+    @Column(name="uf")
     private String uf;
+
+    @Column(name="ibge")
     private String ibge;
+
+    @Column(name="gia")
     private String gia;
+
+    @Column(name="ddd")
     private String ddd;
+
+    @Column(name="siafi")
     private String siafi;
+
+
     private boolean erro = false;
+
     public Endereco() {
     }
 
